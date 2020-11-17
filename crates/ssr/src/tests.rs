4d4f5fd5d13c574c1ae5e6a9b2b8d40f7500f768
Marijn Suijten <marijn@traverseresearch.nl>
2020-11-17 21:47:06 +0100

    ssr: Enable let statement tests that should succeed

--- crates/ssr/src/tests.rs
+++ crates/ssr/src/tests.rs
@@ -173,13 +173,8 @@ fn ssr_let_stmt_in_macro_match() {
 
 #[test]
 fn ssr_let_stmt_in_fn_match() {
-    // FIXME: Optional semicolon parsing works differently than the actually-optional trailing comma
-    // that is only to improve readability when adding new items to a match block, for example.
-    // The ; has a semantic meaning but is expected to be optional in the pattern for ergonomics
-    // (and for the stmt example above), as long as the replacement template follows.
-    // This turns out to be more complicated to do as it is part of the matched LET_STMT syntax node.
-    assert_matches("let $a = 10", "fn main() { let x = 10; x }", &["let x = 10;"]);
-    assert_matches("let $a = $b", "fn main() { let x = 10; x }", &["let x = 10;"]);
+    assert_matches("let $a = 10", "fn main() { let x = 10; x }", &["let x = 10"]);
+    assert_matches("let $a = $b", "fn main() { let x = 10; x }", &["let x = 10"]);
 }
 
 #[test]
@@ -211,19 +206,17 @@ fn ssr_let_stmt_replace() {
     );
 
     // Trailing semicolon only in template
-    // FIXME: This should fail, the expected value must be `let x = 11;;`
     assert_ssr_transform(
         "let $a = $b ==>> let $a = 11;",
         "fn main() { let x = 10; x }",
-        expect![["fn main() { let x = 11; x }"]],
+        expect![["fn main() { let x = 11;; x }"]],
     );
 
     // Trailing semicolon only in pattern
-    // FIXME: This should drop the semicolon
     assert_ssr_transform(
         "let $a = $b; ==>> let $a = 11",
         "fn main() { let x = 10; x }",
-        expect![["fn main() { let x = 11; x }"]],
+        expect![["fn main() { let x = 11 x }"]],
     );
 }
 
@@ -237,11 +230,10 @@ fn ssr_let_stmt_replace_expr() {
     );
 
     // Trailing semicolon should _not_ be dropped from the new expression
-    // FIXME: Trailing semicolon is implicitly matched and dropped
     assert_ssr_transform(
         "let $a = $b ==>> $b",
         "fn main() { let x = 10; }",
-        expect![["fn main() { 10 }"]],
+        expect![["fn main() { 10; }"]],
     );
 }
 
